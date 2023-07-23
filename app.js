7e7ebbb8dce084cf144ee29b5f3c23ab0033ffc6
Cheesypocket <140283226+Cheesypocket@users.noreply.github.com>
2023-07-22 21:44:32 -0400

    Adding Comments
    Adding comments to lesson 5 code in app.js file

--- app.js
+++ app.js
@@ -15,10 +15,29 @@ DisplayOutput(ReadSingleFile(fileDirectory));
 //#endregion executing code
 
 //reads a single file in utf8 format and returns a string value of the stored input
-function ReadSingleFile(fDirectory){
+
+/*
+   * It is important to note how this function is written. Typically a function typed as an action.
+     Say for example: ReadInput(), or CaluclateHypotenuse()
+
+   * Any parameters like any variable should be a noun and should self describe what it is.
+     Say for example: sideLengthA, userInput, or enemyAI.
+
+   * Functions should generally do one task. So here in ReadSingleFile, it literally reads one
+     single file from the directory that is input. It will return the text from said file
+
+   * Also note conventions here. Functions are all capitalized. Variables are camel cased (secondary word is capitalized)
+
+   * Functions are generally important as when put where the code will actually be executed it will:
+     1: reduce the redundency in the code
+     2: make code more readable
+     3: make code easier to debug because of 1 and 2.
+
+*/
+function ReadSingleFile(fileDir){
     const fileSystem = require('fs');
 
-    text = fileSystem.readFileSync(fDirectory, 'utf8');
+    text = fileSystem.readFileSync(fileDir, 'utf8');
 
     return text;
 }
